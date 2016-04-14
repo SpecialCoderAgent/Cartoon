c22644ca95829c6e678a825d1f4082eb64bc963e
soulne <soul.ne@163.com>
2016-04-14 18:51:38 +0800

    项目初始化

---
+++ Cartoon/Cartoon/Other/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  Cartoon
+//
+//  Created by 911 on 16/4/14.
+//  Copyright © 2016年 bin. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    
+    
+    
+    
+    
+    
+    return YES;
+}
+
+
+
+@end
